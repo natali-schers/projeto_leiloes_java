b0df29557afb342d5b38110d583b0b7c4c20b7e9
Natali Schers <schersnatali@gmail.com>
2024-08-21 23:08:40 -0300

    Commit #1: definindo tela inicial do sistema

---
+++ src/LeiloesTDSat.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author natis
+ */
+public class LeiloesTDSat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        cadastroVIEW tp = new cadastroVIEW();
+
+        tp.setVisible(true);
+        tp.setLocationRelativeTo(null);
+    }
+    
+}
